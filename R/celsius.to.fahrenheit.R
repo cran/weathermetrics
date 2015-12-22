c7288cd1adc397695bb56da874899ea83947c0aa
Brooke Anderson <brooke.anderson@colostate.edu>
2015-12-22 23:04:33 +0000

    version 1.2.0

--- R/celsius.to.fahrenheit.R
+++
@@ -1,7 +0,0 @@
-celsius.to.fahrenheit <-
-function (T.celsius, round = 2) 
-{
-    T.fahrenheit <- (9/5) * T.celsius + 32
-    T.fahrenheit <- round(T.fahrenheit, digits = round)
-    return(T.fahrenheit)
-}
--- R/fahrenheit.to.celsius.R
+++
@@ -1,7 +0,0 @@
-fahrenheit.to.celsius <-
-function (T.fahrenheit, round = 2) 
-{
-    T.celsius <- (5/9) * (T.fahrenheit - 32)
-    T.celsius <- round(T.celsius, digits = round)
-    return(T.celsius)
-}
